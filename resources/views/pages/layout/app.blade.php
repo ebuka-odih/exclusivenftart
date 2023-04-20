640713016480ca6e530b95b6fb29d1f8a93de5c2
Gnosis <emmagab38@gmail.com>
2023-04-20 19:24:34 +0100

    commit changes

--- resources/views/pages/layout/app.blade.php
+++ resources/views/pages/layout/app.blade.php
@@ -24,7 +24,7 @@
         @media only screen and (max-width: 600px) {
             .logo-primary {
                 width: 80px;
-                height: 40px;
+                height: 60px;
                 /*max-height: 120px;*/
             }
             .big-logo {
