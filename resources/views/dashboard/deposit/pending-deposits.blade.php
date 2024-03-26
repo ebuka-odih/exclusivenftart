0cd1d45de7fc8aa6c6375b7d3ff7c3e441f7ab43
Gnosis <emmagab38@gmail.com>
2024-03-26 21:48:08 +0100

    commit changes

--- resources/views/dashboard/deposit/pending-deposits.blade.php
+++ resources/views/dashboard/deposit/pending-deposits.blade.php
@@ -61,7 +61,7 @@
                                             <span class="tranx-icon icon ni ni-sign-eth"></span>
                                         </div>
                                         <div class="tranx-data">
-                                            <div class="tranx-label">{{ $item->payment_method->name  }} Deposit <span class="ml-3"> {!! $item->status() !!}</span></div>
+                                            <div class="tranx-label">{{ optional($item->payment_method)->name  }} Deposit <span class="ml-3"> {!! $item->status() !!}</span></div>
                                             <div class="tranx-date">{{ date('M d, Y h:i A', strtotime($item->created_at)) }}</div>
                                             @if($item->status == 0)
                                                 <a href="{{ route('user.payment', $item->id) }}" class="btn btn-sm btn-primary">Pay</a>
